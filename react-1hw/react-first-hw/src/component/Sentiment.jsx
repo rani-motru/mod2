5458edc78e2d6a1457a2326abfd4d88b1e0f185f
rani-motru <rani.motru@icloud.com>
2023-11-07 21:05:09 -0500

    complete Hw

--- react-1hw/react-first-hw/src/component/Sentiment.jsx
+++ react-1hw/react-first-hw/src/component/Sentiment.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 function Sentiment(){
     return (
         <div className="sentiment">
-            <h4>Sentiment Analysis</h4>
-            <h4>960</h4>
+            Sentiment Analysis
+            <center><h4>960</h4>
             <h4>122</h4>
-            <h4>321</h4>
+            <h4>321</h4></center>
         </div>
       )
 }
--- react-1hw/react-first-hw/src/component/Website.jsx
+++ react-1hw/react-first-hw/src/component/Website.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Website() {
   return (
     <div className = "website">
-        website
+        website Visitors
         <h4>821</h4>
         <div className="graph"></div>
     </div>
